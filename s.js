cf711022bc4698e8abe7f8d90c9226e57167e64e
yx0 <3qrcpi+7p29hwia4atms@sharklasers.com>
2016-05-02 14:39:03 +0200

    Update s.js

--- s.js
+++ s.js
@@ -1,6 +1,1 @@
-window.onload = hello; 
-
-function hello() { 
-    var name = prompt("What is your name", "");
-    alert("Hello " + name + "! Welcome to my forum."); 
-}
+window.addEventListener("load", function() { alert("hello!");});
